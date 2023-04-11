1b1f1ac3a59588eea5abfc1c57a78365bd7debb5
qwerty59b <130035937+qwerty59b@users.noreply.github.com>
2023-04-11 18:09:33 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
 DOWNLOAD_LOCATION = "./Downloads"
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "aca va tu token con las comillas")
-APP_ID = int(os.environ.get("API_ID", "aca va tu id con las comillas"))
-API_HASH = os.environ.get("API_HASH", "aca va tu hash con las comillas")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5954160940:AAFwM3TLMACeNxeZjCHINJadsCLVOdyg9Ms")
+APP_ID = int(os.environ.get("API_ID", "7015264"))
+API_HASH = os.environ.get("API_HASH", "9ebbc8ad41951c59b0908cdd2100a158")
 AUTH_GROUP = "" #aca no pongas nada
 
 Creds_path = "creds"
 DOWNLOAD_LOCATION = "/"
 
-adminList = [12345] #aca va el id de tg del administrador del bot y el canal
+adminList = [1153151858] #aca va el id de tg del administrador del bot y el canal
 
 
-EDIT_TIME = 5
+EDIT_TIME = 10
 FINISHED_PROGRESS_STR = "*"
 UNFINISHED_PROGRESS_STR = "_"
