207ab7c0dd0378e5edbf37b131fd591c86eabc0f
qwerty59b <130035937+qwerty59b@users.noreply.github.com>
2023-04-11 18:15:01 -0500

    Update check_channel.py

--- bot/util/check_channel.py
+++ bot/util/check_channel.py
@@ -2,7 +2,7 @@
 
 async def inChannel(client,message):
         try: 
-            await client.get_chat_member("aca pon el nombre de usuario del canal sin @", message.from_user.id)
+            await client.get_chat_member("zzzxdpromax", message.from_user.id)
             print("in channel")
             return True
         except Exception as e:
