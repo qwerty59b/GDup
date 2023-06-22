6fe9e500b7c1c8bfe5931e52b32c9b10654db8d5
qwerty59b <130035937+qwerty59b@users.noreply.github.com>
2023-06-22 03:57:59 -0500

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,4 +1,4 @@
 import os
 
 class Creds():
-    TG_TOKEN = "5954160940:AAFwM3TLMACeNxeZjCHINJadsCLVOdyg9Ms" #Pones el token del bot entre comillas
+    TG_TOKEN = "6099284488:AAENFcuyRMhoFB2jryKooCRD91_l6_ul1EE" #Pones el token del bot entre comillas
